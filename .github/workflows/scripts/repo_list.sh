eb2e896b6a00dad8e5868dfc29b82e73e4ec5c9a
Andy Pearson <andy@londonparkour.com>
2021-06-20 13:01:11 +0100

    Header dynamic

--- .github/workflows/scripts/repo_list.sh
+++ .github/workflows/scripts/repo_list.sh
@@ -2,12 +2,26 @@
 
 GHUSER=IOROOT;
 
-# Grab Repo list for Github API
-/usr/bin/curl "https://api.github.com/users/$GHUSER/repos?per_page=100&sort=created&direction=desc" | /usr/bin/jq '.[] | {NAME: .name, URL: .html_url, DESC: .description, DATE: .created_at}' > ./repos.json
-/usr/bin/curl "https://api.github.com/users/$GHUSER/repos?per_page=100&page=2&sort=created&direction=desc" | /usr/bin/jq '.[] | {NAME: .name, URL: .html_url, DESC: .description, DATE: .created_at}' >> ./repos.json
 
 # Add Header
 /bin/cat ../partials/README_HEAD.md > ./output.md
+
+
+# Get top activity.
+printf "\n## Activity\n" >> ./output.md
+printf "The current repo I'm working on is:" >> ./output.md
+/usr/bin/curl "https://api.github.com/users/$GHUSER/events?per_page=1" | /usr/bin/jq '.[0].repo.name' >> ./output.md
+printf "\n" >> ./output.md
+
+
+# Grab Repo list from Github API
+/usr/bin/curl "https://api.github.com/users/$GHUSER/repos?per_page=100&sort=created&direction=desc" | /usr/bin/jq '.[] | {NAME: .name, URL: .html_url, DESC: .description, DATE: .created_at}' > ./repos.json
+/usr/bin/curl "https://api.github.com/users/$GHUSER/repos?per_page=100&page=2&sort=created&direction=desc" | /usr/bin/jq '.[] | {NAME: .name, URL: .html_url, DESC: .description, DATE: .created_at}' >> ./repos.json
+
+
+# Add to output.
+/bin/cat ../partials/README_CONTENT.md >> ./output.md
+
 
 # Parse it with /usr/bin/jq to make an HTML table
 printf "\n## My Repository List\n" >> ./output.md
