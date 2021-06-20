6977a97815cdfb85781b0ce7a0a5a4a60d759617
Andy Pearson <andy@londonparkour.com>
2021-06-20 12:45:14 +0100

    try again

--- .github/workflows/scripts/repo_list.sh
+++ .github/workflows/scripts/repo_list.sh
@@ -6,11 +6,15 @@ GHUSER=IOROOT;
 /usr/bin/curl "https://api.github.com/users/$GHUSER/repos?per_page=100&sort=created&direction=desc" | /usr/bin/jq '.[] | {NAME: .name, URL: .html_url, DESC: .description, DATE: .created_at}' > ./repos.json
 /usr/bin/curl "https://api.github.com/users/$GHUSER/repos?per_page=100&page=2&sort=created&direction=desc" | /usr/bin/jq '.[] | {NAME: .name, URL: .html_url, DESC: .description, DATE: .created_at}' >> ./repos.json
 
+# Add Header
+/bin/cat ../partials/README_HEAD.md > ./output.md
+
 # Parse it with /usr/bin/jq to make an HTML table
-printf "\n## My Repository List\n" > ./output.md
+printf "\n## My Repository List\n" >> ./output.md
 printf "<table id=\"repos>\">" >> ./output.md
 /bin/cat ./repos.json | /usr/bin/jq --raw-output '"<tr><td><a href=\"\(.URL)\">\(.NAME)</a></td><td>\(.DESC)</td></tr>"' >> ./output.md
 printf "</table>" >> ./output.md
 
-# Cleanup
-mv output.md ../../../README.md
+
+# Copy Results to README.md
+/bin/cat output.md >> ../../../README.md
