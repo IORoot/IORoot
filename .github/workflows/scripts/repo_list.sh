838e1ca000b690fa01a7c11f819d65359587fcdf
Andy Pearson <andy@londonparkour.com>
2021-06-20 12:42:05 +0100

    which JQ

--- .github/workflows/scripts/repo_list.sh
+++ .github/workflows/scripts/repo_list.sh
@@ -7,7 +7,7 @@ GHUSER=IOROOT;
 /usr/bin/curl "https://api.github.com/users/$GHUSER/repos?per_page=100&page=2&sort=created&direction=desc" | /usr/bin/jq '.[] | {NAME: .name, URL: .html_url, DESC: .description, DATE: .created_at}' >> ./repos.json
 
 # Parse it with /usr/bin/jq to make an HTML table
-printf "\n## My Repository List\n" >> ./output.md
+printf "\n## My Repository List\n" > ./output.md
 printf "<table id=\"repos>\">" >> ./output.md
 /bin/cat ./repos.json | /usr/bin/jq --raw-output '"<tr><td><a href=\"\(.URL)\">\(.NAME)</a></td><td>\(.DESC)</td></tr>"' >> ./output.md
 printf "</table>" >> ./output.md
